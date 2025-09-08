4088a16e00aa75c319b54016d9eb50f91c311eff
Richard Shepherd <rshepherd@esri.com>
2025-09-08 17:05:35 +0100

    label: update harfbuzz 11.4.5

--- conanfile_rtc.py
+++ conanfile_rtc.py
@@ -2,7 +2,7 @@ from conans import ConanFile
 
 class HarfbuzzConan(ConanFile):
     name = "harfbuzz"
-    version = "10.2.0"
+    version = "11.4.5"
     url = "https://github.com/Esri/harfbuzz/tree/runtimecore"
     license = "https://github.com/Esri/harfbuzz/blob/master/COPYING"
     description = "HarfBuzz is a text shaping engine."
